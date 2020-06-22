ef721249ee57f3a40524fdabf18e84779be436dd
Legoracer <maxivanec5@gmail.com>
2020-06-22 22:40:46 +0200

    Aerogame requires it for some bullshit

---
+++ src/Client/Controllers/TestController.lua
@@ -0,0 +1,20 @@
+-- Test Controller
+-- Username
+-- June 22, 2020
+
+--// Make sure to do something, testing the Codestream
+
+local TestController = {}
+
+
+function TestController:Start()
+
+end
+
+
+function TestController:Init()
+
+end
+
+
+return TestController
