a9ade0e558699ceced57e30b22796ec11e05be05
Legoracer <maxivanec5@gmail.com>
2020-07-05 23:24:09 +0200

    Adding LoadingScreen behavior

--- src/Client/Controllers/UI/LoadingScreen.lua
+++ src/Client/Controllers/UI/LoadingScreen.lua
@@ -1,8 +1,14 @@
 local LoadingScreen = {}
 
 function LoadingScreen:Start()
-	print(self.Controllers)
-	self.Controllers.Fade:In(0)
+	wait(1)
+	self.Controllers.Fade:In(2)
+	wait(0.5)
+	self.LoadingMenu.Task.Text = "Waiting for data..."
+end
+
+function LoadingScreen:Init()
+	self.LoadingMenu = self.UI:WaitForChild("LoadingMenu")
 end
 
 return LoadingScreen
