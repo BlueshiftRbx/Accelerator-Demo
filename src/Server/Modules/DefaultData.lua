87aac45fc5b14c10cf31d6ec67ec13706aff31f5
soutenu <trujo00.ot@gmail.com>
2020-07-16 02:39:06 -0400

    Updated to reflect data needed in demo

--- src/Server/Modules/DefaultData.lua
+++ src/Server/Modules/DefaultData.lua
@@ -1,44 +1,16 @@
 return {
-	-- NOTE Hub & Session
-	Class = "Commando";
-
-	Levels = {
-		--[[ EXAMPLE LEVEL
-			Class = {
-				Level = 1
-				XPRatio = 0 (between 0 and 1)
-			}
-		]]
-	};
-
-	-- NOTE Hub related
-	Credits = 0; -- Shop currency
-	Trophies = 0; -- maps completed.
-
-	Inventory = {
-		Skins = {};
-		Perks = {};
+	Loadout = {
+		Primary = "SMG";
+		Secondary = "Pistol";
 	};
 
 	__SESSIONDATA = {
 
 	};
 
-	-- NOTE doesn't replicate to the client.
 	__SERVER = {
 		Analytics = {};
 		BanData = {
-			--[[ EXAMPLE BAN
-				Reason = "Ban Example";
-				Timestamp = 0;
-				Duration = "";
-			--]]
 		};
-	};
-
-	-- NOTE Session related
-	Equipped = {
-		Skins = {};
-		Perks = {};
 	};
 }
