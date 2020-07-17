c838f2fcd4a969baed1fa76e78224e16b80096ae
Legoracer <maxivanec5@gmail.com>
2020-07-17 10:20:46 +0200

    Added WeldRelative_Tool

--- src/Shared/Weld.lua
+++ src/Shared/Weld.lua
@@ -21,7 +21,7 @@ function Weld:Weld(i1, i2)
 end
 
 function Weld:WeldRelative_Model(model)
-	local handle = model:FindFirstChild("Handle") or model:FindFirstChildWhichIsA("BasePart", true);
+	local handle = tool.PrimaryPart or tool:FindFirstChild("Handle") or tool:FindFirstChild("_Handle") or model:FindFirstChildWhichIsA("BasePart", true);
 
 	for i,v in pairs(model:GetDescendants()) do
 		if v:IsA('BasePart') and not v == handle then
@@ -30,16 +30,28 @@ function Weld:WeldRelative_Model(model)
 	end
 end
 
+function Weld:WeldRelative_Tool(tool)
+	local handle = tool:FindFirstChild("Handle") or tool:FindFirstChild("_Handle") or tool:FindFirstChildWhichIsA("BasePart", true);
+
+	for i,v in pairs(tool:GetDescendants()) do
+		if v:IsA('BasePart') and not v == handle then
+			self:WeldRelative(handle, v)
+		end
+	end
+end
+
 return setmetatable(Weld, {
 	__call = function(_, instance1, instance2, relative)
-		if instance1:IsA('Model') then
-			rawget(self, "WeldRelative_Model")(instance1)
+		if instance1:IsA('Tool') then
+			rawget(self, "WeldRelative_Tool")(instance1)
 		elseif instance1:IsA('Part') and instance2:IsA('Part') then
 			if relative then
 				rawget(self, "WeldRelative")(instance1, instance2)
 			else
 				rawget(self, "Weld")(instance1, instance2)
 			end
+		else
+			rawget(self, "WeldRelative_Model")(instance1)
 		end
 	end;
 })
