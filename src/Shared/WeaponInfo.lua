b8666ffaf6e3e0f8f852faf98be5a4a8578aba52
soutenu <trujo00.ot@gmail.com>
2020-07-17 01:09:07 -0400

    Setup basic data

--- src/Shared/WeaponInfo.lua
+++ src/Shared/WeaponInfo.lua
@@ -1,10 +1,24 @@
 local WeaponInfo = {
 	["SMG"] = {
+		ClipSize = 30;
+		FireRate = 0.05;
 
+		Anims = {
+			HoldAnim = "";
+			FiringAnim = "";
+			ReloadingAnim = "";
+		};
 	};
 
 	["Pistol"] = {
+		ClipSize = 12;
+		FireRate = 1;
 
+		Anims = {
+			HoldAnim = "";
+			FiringAnim = "";
+			ReloadingAnim = "";
+		};
 	};
 }
 
