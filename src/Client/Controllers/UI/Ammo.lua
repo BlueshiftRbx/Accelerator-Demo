0f8f265c00e4092c52fb0bb424208597099f872c
Legoracer <maxivanec5@gmail.com>
2020-07-17 09:20:35 +0200

    Added Ammo UI wrapper functions

--- src/Client/Controllers/UI/Ammo.lua
+++ src/Client/Controllers/UI/Ammo.lua
@@ -1,12 +1,15 @@
 local Ammo = {};
 
-function Ammo:Start()
+function Ammo:SetAmmo(ammo)
+	self.AmmoUI.Text = ("%d / ∞"):format(ammo)
+end
 
+function Ammo:SetVisible(visible)
+	self.AmmoUI.Visible = visible;
 end
 
 function Ammo:Init()
-
+	self.AmmoUI = self.UI:WaitForChild("GameMenu"):WaitForChild("Ammo")
 end
-
 
 return Ammo;
