b7d7dabfb635d0df49496b6a339250f4c9df86ab
soutenu <trujo00.ot@gmail.com>
2020-07-18 17:17:36 -0400

    Updated to use Animations folder

--- src/Shared/Assets.lua
+++ src/Shared/Assets.lua
@@ -1,14 +1,11 @@
 -- Services
 local RunService = game:GetService("RunService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Debris = game:GetService("Debris")
 local ServerStorage
-
--- Modules
-local Animations = require(script:WaitForChild("Animations"))
 
 -- Shared references
 local sharedAssetsFolder = ReplicatedStorage:WaitForChild("Assets")
+local animationsFolder = sharedAssetsFolder:WaitForChild("Animations")
 local particlesFolder = sharedAssetsFolder:WaitForChild("Particles")
 local soundsFolder = sharedAssetsFolder:WaitForChild("Sounds")
 
@@ -19,7 +16,7 @@ local toolsFolder
 
 local Assets = {}
 
-function Assets:GetParticle(name, duration)
+function Assets:GetParticle(name)
 	assert(type(name) == "string", "Name must be a string")
 
 	local particle = particlesFolder:FindFirstChild(name)
@@ -27,25 +24,17 @@ function Assets:GetParticle(name, duration)
 	if particle then
 		particle = particle:Clone()
 
-		if duration then
-			Debris:AddItem(particle, duration)
-		end
-
 		return particle
 	end
 end
 
-function Assets:GetSound(name, duration)
+function Assets:GetSound(name)
 	assert(type(name) == "string", "Name must be a string")
 
 	local sound = soundsFolder:FindFirstChild(name)
 
 	if sound then
 		sound = sound:Clone()
-
-		if duration then
-			Debris:AddItem(sound, duration)
-		end
 
 		return sound
 	end
@@ -54,20 +43,17 @@ end
 function Assets:GetAnimation(name)
 	assert(type(name) == "string", "Name must be a string")
 
-	local animId = Animations[name]
+	local animation = animationsFolder:FindFirstChild(name)
 
-	if animId then
-		local animation = Instance.new("Animation")
-
-		animation.Name = name
-		animation.AnimationId = animId
+	if animation then
+		animation = animation:Clone()
 
 		return animation
 
 	end
 end
 
-function Assets:GetEntity(name, duration)
+function Assets:GetEntity(name)
 	assert(type(name) == "string", "Name must be a string")
 	assert(RunService:IsServer() == true, "Assets:GetEntity must be called on the server")
 
@@ -75,10 +61,6 @@ function Assets:GetEntity(name, duration)
 
 	if entity then
 		entity = entity:Clone()
-
-		if duration then
-			Debris:AddItem(entity, duration)
-		end
 
 		return entity
 	end
--- src/Shared/Assets/Animations.lua
+++
@@ -1,4 +0,0 @@
-return {
-	["SMG_HOLD"] = "rbxassetid://5368735024";
-	["SMG_FIRE"] = "rbxassetid://5368738423";
-}
