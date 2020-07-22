2e5d27418d3ec848956263b95ca7f41899e541a5
soutenu <trujo00.ot@gmail.com>
2020-07-22 04:35:34 -0400

    Added to Entity Class

--- src/Server/Modules/Entity.lua
+++ src/Server/Modules/Entity.lua
@@ -1,18 +1,23 @@
-local Entity = {};
-Entity.__index = Entity;
+local Entity = {}
+Entity.__index = Entity
 
-function Entity.new(entityObject)
-	local self = setmetatable({
-		Model = entityObject;
-	}, Entity)
+function Entity.new(entityObject, info)
+    local self = setmetatable({}, Entity)
 
-	self.Info = self.Shared.EntityInfo[entityObject.Name]
+    self.Model = entityObject
+    self.Humanoid = entityObject:WaitForChild("Humanoid")
 
-	return self
+    return self
+end
+
+function Entity:TakeDamage(damage)
+    if damage > 0 then
+
+    end
 end
 
 function Entity:Step()
 
 end
 
-return Entity;
+return Entity
