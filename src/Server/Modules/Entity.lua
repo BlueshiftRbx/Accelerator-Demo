bf50e00c06db7a5ac14a8a82cd41c298065c8108
soutenu <trujo00.ot@gmail.com>
2020-07-24 09:47:29 -0400

    AI System Refactor

--- src/Server/Modules/Entity.lua
+++ src/Server/Modules/Entity.lua
@@ -1,96 +1,178 @@
+-- Services
+local Players = game:GetService("Players")
+local Workspace = game:GetService("Workspace")
+local PathfindingService = game:GetService("PathfindingService")
+local AIService
+
+-- Modules
+local Maid
+
+-- References
+local zombiesFolder = Workspace:WaitForChild("Zombies")
+
+-- Constants
+local VALID_STATUS = {
+	[Enum.PathStatus.Success] = true;
+	[Enum.PathStatus.ClosestNoPath] = true;
+	[Enum.PathStatus.ClosestOutOfRange] = true;
+}
+
+-- Methods
+function Raycast(position1, position2, target)
+	if position1 and position2 and target then
+		local position = position1
+		local direction = (position2 - position1).Unit * (position2 - position1).Magnitude
+
+		local noneTargetPlayers = {}
+
+		for _,player in next, Players:GetPlayers() do
+			if player.Character and player.Character ~= target then
+				table.insert(noneTargetPlayers, player.Character)
+			end
+		end
+
+		local rayParams = RaycastParams.new()
+
+		rayParams.FilterDescendantsInstances = {zombiesFolder ,unpack(noneTargetPlayers)}
+		rayParams.FilterType = Enum.RaycastFilterType.Blacklist
+		rayParams.IgnoreWater = true
+
+		local castResult = Workspace:Raycast(position, direction, rayParams)
+
+		return castResult
+	end
+end
+
 local Entity = {}
 Entity.__index = Entity
 
-local Players = game:GetService("Players")
-
 function Entity.new(entityObject, info)
-    local self = setmetatable({}, Entity)
+	local self = setmetatable({}, Entity)
 
-    self.Model = entityObject
-    self.Humanoid = entityObject:WaitForChild("Humanoid")
-    self.Root = entityObject:WaitForChild("HumanoidRootPart")
-    self.Info = info;
+	self.Maid = Maid.new()
 
-    self.Model:SetPrimaryPartCFrame(self:FindSpawnLocation())
-    self.Model.Parent = workspace:WaitForChild("Zombies")
+	self.Path = PathfindingService:CreatePath({
+		AgentRadius = 2;
+		AgentHeight = 5;
+		AgentCanJump = false;
+	})
 
-    spawn(function()
-        while not self._Destroyed do
-            wait(0.25)
-            self:Step()
-        end
-    end)
+	self.Character = entityObject
+	self.Humanoid = entityObject:WaitForChild("Humanoid")
+	self.Info = info
 
-    return self
+	self.Target = nil
+	self.IsMoving = false
+	self.IsDestroyed = false
+
+	self.Maid:GiveTask(self.Path)
+	self.Maid:GiveTask(self.Humanoid)
+	self.Maid:GiveTask(self.Character)
+
+	-- Initialize
+	self.Character.Parent = zombiesFolder
+
+	return self
 end
 
-function Entity:FindSpawnLocation()
-    return CFrame.new(-77, 8.439, 69)
+function Entity:SetTarget(newTarget)
+	if newTarget and newTarget:IsA("Model") then
+		if self.Target ~= newTarget then
+			if self.isMoving then
+				self.isMoving = false
+			end
+
+			print("New target:", newTarget)
+
+			self.Target = newTarget
+		end
+	end
+end
+
+function Entity:Step()
+	self:DeterminePath()
+end
+
+function Entity:DeterminePath()
+	if self.Target and self.Target:IsDescendantOf(Workspace) then
+		local humanoid = self.Target:FindFirstChildOfClass("Humanoid")
+
+		if humanoid and humanoid.Health > 0 then
+			local castResult = Raycast(self.Humanoid.RootPart.Position, humanoid.RootPart.Position, self.Target)
+
+			if castResult and castResult.Instance:IsDescendantOf(self.Target) then
+				self:MoveTo()
+			else
+				self:TravelPath()
+			end
+		end
+	end
+end
+
+function Entity:MoveTo()
+	if self.Target and self.Target:IsDescendantOf(Workspace) then
+		if self.Humanoid and self.Humanoid:IsDescendantOf(Workspace) then
+			local humanoid = self.Target:FindFirstChildOfClass("Humanoid")
+
+			if humanoid and humanoid.Health > 0 then
+				self.IsMoving = true
+
+				self.Humanoid:MoveTo(humanoid.RootPart.Position)
+			end
+		end
+	end
+end
+
+function Entity:TravelPath()
+	if self.Target and self.Target:IsDescendantOf(Workspace) then
+		if self.Humanoid and self.Humanoid:IsDescendantOf(Workspace) then
+			local humanoid = self.Target:FindFirstChildOfClass("Humanoid")
+
+			if humanoid and humanoid.Health > 0 then
+				self.Path:ComputeAsync(self.Humanoid.RootPart.Position, humanoid.RootPart.Position)
+				if VALID_STATUS[self.Path.Status] then
+					print("Valid path status")
+					self.IsMoving = true
+
+					for _,waypoint in next, self.Path:GetWaypoints() do
+						if not self.IsMoving then
+							break
+						end
+
+						self.Humanoid:MoveTo(waypoint.Position)
+						self.Humanoid.MoveToFinished:Wait()
+					end
+
+					self.IsMoving  = false
+				end
+			end
+		end
+	end
 end
 
 function Entity:TakeDamage(damage)
-    if damage > 0 then
-        self.Health = self.Health - math.clamp(damage, 0, self.Health);
+	if damage > 0 then
+		self.Humanoid:TakeDamage(damage)
 
-        if self.Health == 0 then
-            self:Destroy()
-        end
-    end
+		if self.Humanoid.Health <= 0 then
+			self:Destroy()
+		end
+	end
 end
 
 function Entity:Destroy()
-    -- Play death animation
-    -- Ragdoll?
-    -- Delete after X seconds
-
-    self._Destroyed = true
+	if not self.IsDestroyed then
+		self.IsDestroyed = true
+		-- Play death animation
+		-- Ragdoll?
+		-- Delete after X seconds
+		self.Maid:DoCleaning()
+	end
 end
 
-
-
-function Entity:Step()
-    -- Actual game will have flock movements to make sure NPCs dont hit eachother and to lessen the server stress
-    -- Maybe also add "aggro" so that the most damage-dealing player gets priority of getting eaten
-
-    local function getClosestPlayer()
-        local closestPlayer, magnitude;
-
-        for _,player in pairs(Players:GetPlayers()) do
-            local character = player.Character;
-            local root = character and character:FindFirstChild("HumanoidRootPart")
-
-            if character and root then
-                if closestPlayer == nil then
-                    local nMagnitude = (root.Position - self.Root.Position).Magnitude
-                    if nMagnitude < self.Info.FollowDistance then
-                        closestPlayer = player;
-                        magnitude = nMagnitude
-                    end
-                else
-                    local nMagnitude = (root.Position - self.Root.Position).Magnitude
-                    if nMagnitude < magnitude and nMagnitude < self.Info.FollowDistance then
-                        closestPlayer = player;
-                        magnitude = nMagnitude;
-                    end
-                end
-            end
-        end
-
-        return closestPlayer, magnitude
-    end;
-
-    local closestPlayer, magnitude = getClosestPlayer();
-    if closestPlayer then
-
-        if magnitude < self.Info.AttackDistance then
-            -- Attack
-        else
-            local closestPlayerRoot = (closestPlayer.Character and closestPlayer.Character:FindFirstChild("HumanoidRootPart"))
-            self.Humanoid:MoveTo(
-                closestPlayerRoot.Position
-            )
-        end
-    end
+function Entity:Init()
+	AIService = self.Services.AIService
+	Maid = self.Shared.Maid
 end
 
 return Entity
